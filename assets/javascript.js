a4d21ae6e9cf32e807131f44b330023ade3236cd
IsraelMuCa <isra.munoz@gmail.com>
2018-04-27 13:20:50 -0500

    Rearranged the javascript file to make it more understandable and prepared for future refactoring

--- assets/javascript.js
+++ assets/javascript.js
@@ -25,29 +25,12 @@
     }
 */
 
-
-
 //Declare all my variables outside of the functions
 var machineChoice;
 var userChoice;
 var tieScore = 0;
 var userScore = 0;
 var machineScore = 0;
-
-/*Functions to keep score
-Most surely there's a way to do it all on one function, will do it on 3 different functions for now
-*/
-function noOneScored() {
-    tieScore+=1;
-}
-
-function userScored() {
-    userScore+=1;
-}
-
-function machineScored(){
-    machineScore+=1;
-}
 
 //Function to make the machine select an option randomly
 function machineSelection() {
@@ -64,30 +47,9 @@ function machineSelection() {
     }
 }
 
+// - - USER SELECTION FUNCTIONS - -
 
-/*Functions to save the possibilities
-//Ideally they would receive both the user choice and the machine choice to show it.
-To be worked at a later date */
-function tie(userChoice, machineChoice) {
-    alert("You chose " + userChoice + " and the machine chose " + machineChoice + "\n Sooo, this is a tie!")
-    this.noOneScored();
-    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
-}
-
-function userWins(userChoice, machineChoice) {
-    alert("You chose " + userChoice + " and the machine chose " + machineChoice + "\n Sooo, you won!")
-    this.userScored();
-    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
-}
-
-function machineWins(userChoice, machineChoice) {
-    alert("You chose " + userChoice + " and the machine chose " + machineChoice + "\n Sooo, the machine won!")
-    this.machineScored();
-    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
-}
-
-
-//covers user choosing rock
+//Function for when the user chooses rock
 function userRock(){
     userChoice = "rock";
     this.machineSelection();
@@ -103,7 +65,7 @@ function userRock(){
     }
 }
 
-//cover userScissors
+//Function for when the user chooses scissors
 function userScissors(){
     userChoice = "scissors";
     this.machineSelection();
@@ -119,7 +81,7 @@ function userScissors(){
     }
 }
 
-//cover userPaper
+//Function for when the user chooses paper
 function userPaper(){
     userChoice = "paper";
     this.machineSelection();
@@ -133,4 +95,37 @@ function userPaper(){
         //Here for testing purposes if needed
         //console.log(machineChoice);
     }
+}
+
+//Functions for each possibility, with alerts for the user
+function tie(userChoice, machineChoice) {
+    alert("You chose " + userChoice + " and the machine chose " + machineChoice + "\nSooo, this is a tie!")
+    this.noOneScored();
+    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
+}
+
+function userWins(userChoice, machineChoice) {
+    alert("You chose " + userChoice + " and the machine chose " + machineChoice + "\nSooo, you won!")
+    this.userScored();
+    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
+}
+
+function machineWins(userChoice, machineChoice) {
+    alert("You chose " + userChoice + " and the machine chose " + machineChoice + "\nSooo, the machine won!")
+    this.machineScored();
+    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
+}
+
+/* Functions to keep score
+Most surely there's a way to do it all in one function, will do it on 3 different functions for now */
+function noOneScored() {
+    tieScore+=1;
+}
+
+function userScored() {
+    userScore+=1;
+}
+
+function machineScored() {
+    machineScore+=1;
 }
