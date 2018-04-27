ca8c1cdd280bb1226469dffa8d986a20a35e1e0c
IsraelMuCa <isra.munoz@gmail.com>
2018-04-27 11:50:39 -0500

    Added functions for score keeping

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,6 @@
 /*
     PSEUDOCODE - Here for legacy reasons
+
     1) Start with a screen with 3 options
     1.1) have 3 buttons
     2) Get user input for which of the 3 options he'd like to play with
@@ -28,9 +29,24 @@
 
 //Declare all my variables outside of the functions
 var machineChoice;
+var tieScore = 0;
+var userScore = 0;
+var machineScore = 0;
 
-/*Function to keep score
-To be worked at a later date */
+/*Functions to keep score
+Most surely there's a way to do it all on one function, will do it on 3 different functions for now
+*/
+function noOneScored() {
+    tieScore+=1;
+}
+
+function userScored() {
+    userScore+=1;
+}
+
+function machineScored(){
+    machineScore+=1;
+}
 
 //Function to make the machine select an option randomly
 function machineSelection() {
@@ -53,14 +69,20 @@ function machineSelection() {
 To be worked at a later date */
 function tie() {
     alert("This is a tie!")
+    this.noOneScored();
+    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
 }
 
 function userWins() {
     alert("You won!")
+    this.userScored();
+    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
 }
 
 function machineWins() {
     alert("The machine won!")
+    this.machineScored();
+    alert("The current score is: \n" + tieScore + " ties. \n" + userScore + " wins by the user. \n" + machineScore + " wins by the machine.")
 }
 
 
